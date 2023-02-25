4e162efc7e88061f5cc3859742c22a8a8b76dc04
wannagetout <inv.moiseev@gmail.com>
2023-02-25 10:15:56 +0300

    Added console output in main file

--- main.py
+++ main.py
@@ -2,28 +2,18 @@ from node import Node
 from stack import Stack
 
 if __name__ == '__main__':
-	a1 = Node(1, None)
-	a2 = Node(81, a1)
-
-	print(a1.data)
-	print(a2.data)
-	print(a1)
-	print(a2.next_data)
 
 	stack = Stack()
+	stack.push('data1')
+	data = stack.pop()
 
+	print(stack.head)
+	print(data)
+
+	stack = Stack()
 	stack.push('data1')
 	stack.push('data2')
-	stack.push('data3')
+	data = stack.pop()
 
-	print('1')
 	print(stack.head.data)
-	print('2')
-	print(stack.head.next_data.data)
-	print('3')
-	print(stack.head.next_data.next_data.data)
-	print('4')
-	print(stack.head.next_data.next_data.next_data)
-	print('5')
-	print(stack.head.next_data.next_data.next_data.data)
-
+	print(data)
