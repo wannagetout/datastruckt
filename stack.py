2937d01b962ea3806de22649ca26e37c4c99e308
wannagetout <inv.moiseev@gmail.com>
2023-02-25 10:11:11 +0300

    Implemented pop method, tests coverage - 100%

--- stack.py
+++ stack.py
@@ -10,3 +10,9 @@ class Stack:
 		next_node = self.head
 		new_head = Node(value, next_node)
 		self.head = new_head
+
+	def pop(self):
+		pop_head = self.head
+		pop_data = pop_head.data
+		self.head = pop_head.next_data
+		return pop_data
--- tests/tests.py
+++ tests/tests.py
@@ -7,18 +7,9 @@ from node import Node
 
 class TestNode(unittest.TestCase):
 
-	def test_node_structure(self):
-		node1 = Node(1, None)
-		node2 = Node(90, node1)
+	def test_Stack(self):
 		stack = Stack()
 		stack.push('data1')
 		stack.push('data2')
-		stack.push('data3')
-		self.assertEqual(node1.data, 1)
-		self.assertEqual(node2.data, 90)
-		self.assertEqual(type(node1), node.Node)
-		self.assertEqual(type(node2), node.Node)
-		self.assertEqual(stack.head.data, 'data3')
-		self.assertEqual(stack.head.next_data.data, 'data2')
-		self.assertEqual(stack.head.next_data.next_data.data, 'data1')
-		self.assertEqual(stack.head.next_data.next_data.next_data, None)
+		self.assertEqual(stack.head.data, 'data2')
+		self.assertEqual(stack.pop(), 'data2')
